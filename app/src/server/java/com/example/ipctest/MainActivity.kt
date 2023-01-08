ab96ba6ace2e717fcfe8e0eb6fabf95c15dddb96
samer <samer_naoura@outlook.com>
2023-01-08 18:19:11 +0000

    Fix register flag in the manager

---
+++ app/src/server/java/com/example/ipctest/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.ipctest
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.ipctest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+
+}
